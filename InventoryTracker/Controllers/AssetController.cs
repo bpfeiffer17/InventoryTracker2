8449c2120adb763bbf60249d09f515f698767a8f
bpfeiffer17@jcu.edu <bpfeiffer17@jcu.edu>
2017-09-16 14:24:41 -0400

    Added an Asset Controller and a Browse view for that controller

---
+++ InventoryTracker/Controllers/AssetController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryTracker.Controllers
+{
+    public class AssetController : Controller
+    {
+        // GET: Asset
+        public ActionResult Index()
+        {
+            return View("Browse");
+        }
+
+        public ActionResult Browse()
+        {
+            return View();
+        }
+    }
+}
