debaaa4c19e2c382ff7fa449e99593c0670e3218
bpfeiffer17@jcu.edu <bpfeiffer17@jcu.edu>
2017-09-17 15:59:29 -0400

    Added a fake end point for asset types. Displaying those asset types in a table on the Browse view

--- InventoryTracker/Scripts/AssetType/Browse.js
+++ InventoryTracker/Scripts/AssetType/Browse.js
@@ -1,3 +1,20 @@
-﻿$(document).ready(function() {
-    console.log('made it');
+﻿/**
+ * $(document).ready waits for the all html content to be loaded
+ * and then executes the code inside the callback function.
+ */
+$(document).ready(function () {
+    //What do we want to do when the page is loaded?
+    //We want to gather asset types from the db
+    $.get('/Scripts/DummyData/AssetTypes.json', function (data, status) {
+        for (var assetType of data) {
+            var row = $('<tr></tr>');
+            var nameCol = $('<td>' + assetType.name + '</td>');
+            var descCol = $('<td>' + assetType.description + '</td>');
+            row.append(nameCol);
+            row.append(descCol);
+            $('#assetTypesTable tbody').append(row);
+        }
+    });
+    //Display those assets types in a table
+    //Give the user options for those asset types
 });
