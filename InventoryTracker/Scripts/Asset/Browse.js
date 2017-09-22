4cea4dee8a2bfea18a625a3377190d2b8bb185f4
Bennett Lazor <blazor18@jcu.edu>
2017-09-22 10:57:32 -0400

    Added a table to  the assets page

--- InventoryTracker/Scripts/Asset/Browse.js
+++ InventoryTracker/Scripts/Asset/Browse.js
@@ -1,1 +1,25 @@
 ﻿
+
+$(document).ready(function (){
+
+    $.get('/Scripts/DummyData/Assets.json', function (data, status){
+
+    for(var assets of data){
+        //create a row
+        var row = $('<tr></tr>');
+        //colmuns
+        var assetTypeCol = $('<td>' + assets.assetType+ '</td>');
+        var nameCol = $('<td>' + assets.name + '</td>');
+        var descCol = $('<td>' + assets.description + '</td>');
+        //Append the columns to the row
+        row.append(assetTypeCol);
+        row.append(nameCol);
+        row.append(descCol);
+        //Append the row the the tables body
+          $('#assetsTable tbody').append(row);
+
+         }
+         $('#assetsTable').DataTable();
+    });
+
+});
