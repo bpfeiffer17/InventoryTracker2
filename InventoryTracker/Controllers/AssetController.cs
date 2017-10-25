e3556ce2b07327fd4fbe615e169766d82c9a91fe
Brandon Pfeiffer <brandon@comsat-architects.com>
2017-10-25 11:17:43 -0400

    asset controller fix

--- InventoryTracker/Controllers/AssetController.cs
+++ InventoryTracker/Controllers/AssetController.cs
@@ -1,4 +1,5 @@
 ﻿using LumenWorks.Framework.IO.Csv;
+using InventoryTracker.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,26 +12,45 @@ namespace InventoryTracker.Controllers
 {
     public class AssetController : Controller
     {
+        private InventoryTrackerEntities db = new InventoryTrackerEntities();
+
         // GET: Asset
         public ActionResult Index()
         {
             return View("Browse");
         }
 
-        public ActionResult Browse()
+        public ActionResult Browse(int id = 0)
         {
+            ViewBag.assetTypes = db.AssetTypes.ToList();
+            if (id != 0)
+            {
+                Request.Cookies["UserSettings"]["AssetTypeToBrowseID"] = id.ToString();
+                ViewBag.assetTypeToBrowse = db.AssetTypes.Find(id);
+            }
+            if (Request.Cookies["UserSettings"]["AssetTypeToBrowseID"] != null)
+            {
+                ViewBag.assetTypeToBrowse = db.AssetTypes.Find((Int64.Parse(Request.Cookies["UserSettings"]["AssetTypeToBrowseID"])));
+            }
+            else
+            {
+                ViewBag.assetTypeToBrowse = ViewBag.assetTypes[0];
+            }
+            //Gather a list of Assets from the database
+            ViewBag.assets = db.Assets.ToList();
             return View();
         }
 
-		public ActionResult Edit(int id)
+        public ActionResult Edit(int id)
 		{
             ViewBag.id = id;
             return View();
 		}
+
         public ActionResult View(int id)
         {
-            ViewBag.id = id;
-            return View();
+            Asset asset = db.Assets.Find(id);
+            return View(asset);
         }
 
         public ActionResult BulkImport()
